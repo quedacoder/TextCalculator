8871f800150278f43c4193792baffd63da958c96
quedacoder <quedacoder@gmail.com>
2019-12-08 11:16:00 -0600

    Initial Setup

---
+++ src/com/quedacoder/CalculatorApp.java
@@ -0,0 +1,17 @@
+package com.quedacoder;
+
+/**
+ * Main program for the Text Calculator Application
+ * Users will be able use Text Calculator to perform
+ * Addition, Subtraction, Multiplecation, division,
+ * Modulo Division, Raise to Any Power, get Any Root
+ *
+ */
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
