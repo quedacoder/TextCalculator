b04c1ebf4951df4470b6c8b79e6f92b6c89b8b59
quedacoder <quedacoder@gmail.com>
2019-12-16 19:20:24 -0600

    create the custom exception InvalidCalculatorOperation

---
+++ src/com/quedacoder/exceptions/InvalidCalculatorOperation.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.quedacoder.exceptions;
+
+/**
+ * @author quedacoder
+ *
+ */
+public class InvalidCalculatorOperation extends Exception {
+	
+	public InvalidCalculatorOperation(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+
+}
